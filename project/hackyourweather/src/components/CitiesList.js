239aee2bb192229b7ef083a370bff796cbf35064
Anis Alkomem <anis.alkumaim@gmail.com>
2020-07-27 13:50:30 +0200

    Minor fix

--- project/hackyourweather/src/components/CitiesList.js
+++ project/hackyourweather/src/components/CitiesList.js
@@ -1,11 +1,9 @@
 import React from "react";
 import City from "./City";
 
-function CitiesList({ cities, isError, errorMessage, deleteHandle }) {
+function CitiesList({ cities, deleteHandle }) {
   return (
     <>
-      {isError && <div className="error-message">{errorMessage}</div>}
-
       {cities.map((city, index) => {
         return <City city={city} key={index} deleteHandle={deleteHandle} />;
       })}
